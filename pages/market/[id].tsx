3fe3dcbffe5e9dae3984550b97a0d9c2edce9378
TrungHuy1911 <huydthe150252@fpt.edu.vn>
2022-06-29 21:30:32 +0700

    fix: market detail page

--- pages/market/[id].tsx
+++ pages/market/[id].tsx
@@ -2,46 +2,42 @@ import type { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import CardDetail from "components/pages/CardDetail";
 import { Card } from "../../models/Card";
 
-
-
 type Props = {
-    cardDetail: Card;
-}
+  card: Card;
+};
 
 type Params = {
-    id: string;
-}
+  id: string;
+};
 
-const CardDetailPage: NextPage<Card> = (cardDetail: Card) => <CardDetail cardDetail={cardDetail} />;
-
-
+const CardDetailPage: NextPage<Card> = () => <CardDetail />;
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-    const exampleAPIResponse = [1, 2, 3, 6, 7, 10];
+  const exampleAPIResponse = [1, 2, 3, 6, 7, 10];
 
-    const paths = exampleAPIResponse.map((id) => ({
-        params: { id: id.toString() },
-    }));
+  const paths = exampleAPIResponse.map((id) => ({
+    params: { id: id.toString() },
+  }));
 
-    return { paths, fallback: false };
-}
+  return { paths, fallback: false };
+};
 
 export const getStaticProps: GetStaticProps<Props, Params> = ({ params }) => {
-    if (params === undefined) {
-        return {
-            notFound: true,
-        };
-    }
-
-    const { id } = params;
-    // TODO: fetch data from API with this id
-    const data = { id, name: "Example Title", price: 1000, image: "Test" };
-
+  if (params === undefined) {
     return {
-        props: {
-            cardDetail: data
-        },
-    }
-}
+      notFound: true,
+    };
+  }
+
+  const { id } = params;
+  // TODO: fetch data from API with this id
+  const data = { id, name: "Example Title", price: 1000, image: "Test" };
+
+  return {
+    props: {
+      card: data,
+    },
+  };
+};
 
 export default CardDetailPage;
